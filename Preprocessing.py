3e58102d842410459fc263d15331772c23da3c41
Alessia <alessia.lombarda@studenti.unimi.it>
2020-06-17 23:44:23 +0200

    Correzioni al file per preprocessing

--- Preprocessing.py
+++ Preprocessing.py
@@ -4,6 +4,7 @@ import sys
 
 
 data = pd.read_csv(str(sys.argv[1]), sep=',')
+print(data)
 colname = str(sys.argv[2])
 le = preprocessing.LabelEncoder()
 target=le.fit_transform(data[colname])
@@ -11,7 +12,8 @@ target=le.fit_transform(data[colname])
 data[colname]=target
 
 cols = list(data.columns.values)
+print(cols)
 cols.pop(cols.index(colname))
 data = data[[colname]+cols]
-
-data.to_csv('iris_std.csv')
+print(data)
+data.to_csv('iris_std.csv', index=False)
