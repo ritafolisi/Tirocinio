3c2de6c08aab8e8941b8948207e900ba400069b1
Alessia <alessia.lombarda@studenti.unimi.it>
2020-06-23 22:45:22 +0200

    Fix generatore di file per classi

--- iris_generator.py
+++ iris_generator.py
@@ -7,6 +7,8 @@ colname = "species"
 cols = list(df.columns.values)
 cols.pop(cols.index(colname))
 df = df[[colname]+cols]
+df1=df.copy()
+df2=df.copy()
 
 df.replace(to_replace="setosa", value=1, inplace=True)
 df.replace(to_replace='virginica', value=0, inplace=True)
@@ -14,13 +16,16 @@ df.replace(to_replace='versicolor', value=0, inplace=True)
 
 df.to_csv('iris-setosa.csv', index=False)
 
-df.replace(to_replace="setosa", value=0, inplace=True)
-df.replace(to_replace='virginica', value=1, inplace=True)
+df = pd.read_csv ("iris.csv")
 
-df.to_csv('iris-virginica.csv', index=False)
+df1.replace(to_replace="setosa", value=0, inplace=True)
+df1.replace(to_replace='virginica', value=1, inplace=True)
+df1.replace(to_replace='versicolor', value=0, inplace=True)
 
+df1.to_csv('iris-virginica.csv', index=False)
 
-df.replace(to_replace='virginica', value=0, inplace=True)
-df.replace(to_replace='versicolor', value=1, inplace=True)
+df2.replace(to_replace="setosa", value=0, inplace=True)
+df2.replace(to_replace='virginica', value=0, inplace=True)
+df2.replace(to_replace='versicolor', value=1, inplace=True)
 
-df.to_csv('iris-versicolor.csv', index=False)
+df2.to_csv('iris-versicolor.csv', index=False)
