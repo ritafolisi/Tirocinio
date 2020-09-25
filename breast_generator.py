ee6789cc7b32360903d84a06df2d519c420e5886
ritafolisi <ritafolisi@studenti.unimi.it>
2020-09-25 18:22:14 +0200

    Aggiunto script per generazione di breast

--- breast_generator.py
+++ breast_generator.py
@@ -1,25 +1,31 @@
 
 import pandas as pd
+from sklearn import preprocessing
 
 df = pd.read_csv ("Dataset/breast-cancer.csv")
 
+# Posiziono la classe come prima colonna
 colname = "class"
 cols = list(df.columns.values)
 cols.pop(cols.index(colname))
 df = df[[colname]+cols]
 
-df.replace(to_replace="recurrence-events", value=1, inplace=True)
-df.replace(to_replace='false-recurrence-events', value=0, inplace=True)
+# Sostituisco gli intervalli con il valore medio
+colnames = ['age', 'tumor-size', 'inv-falsedes']
+for colname in colnames:
+    conta=0
+    for i in df[colname]:
+        x = i.split("-")
+        df.at[conta, colname] = (int(x[0]) + int (x[1]))/2
+        conta +=1
 
-df.replace(to_replace='ge40', value=0, inplace=True)
-df.replace(to_replace='premefalse', value=1, inplace=True)
-df.replace(to_replace='lt40', value=2, inplace=True)
 
-df.replace(to_replace='True', value=1, inplace=True)
-df.replace(to_replace='False', value=0, inplace=True)
+colnames = ['class', 'mefalsepause', 'falsede-caps', 'breast', 'irradiat',  ]
+for colname in colnames:
+    le = preprocessing.LabelEncoder()
+    target=le.fit_transform(df[colname])
 
-df.replace(to_replace='left', value=1, inplace=True)
-df.replace(to_replace='right', value=1, inplace=True)
+    df[colname]=target
 
 df.replace(to_replace='left_up', value=0, inplace=True)
 df.replace(to_replace='central', value=1, inplace=True)
